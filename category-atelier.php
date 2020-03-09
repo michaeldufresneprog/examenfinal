3a2823f35a8fb0bbae83aa326f8a0e30ac781551
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-03-09 09:17:26 -0400

    question-2

--- category-atelier.php
+++ category-atelier.php
@@ -3,13 +3,14 @@
 <section class="container section-nouvelle">
         <?php
         $query = new WP_Query( array( 'category_name' => "atelier",
+        'order' => 'ASC'
         ));
         if ( $query->have_posts() ) {
             while ( $query->have_posts() ) {
                     $query->the_post();
 					?><div class="div-atelier"class = "structure-atelier">
                         <?php
-                            echo '<p class="p-tittle"><a href="'.get_permalink( $id ).'">' . get_the_title() .  '</a></p>';
+                            echo '<p class="p-tittle"><a href="'.get_permalink( $id ).'">' . get_the_title() . "___" . "<span class='span-1'>" . get_post_field('post_name') . "</span>" . "___" . "<span class='span-2'>" . get_the_author_meta( 'display_name', $post->post_author )   . "</span>" . '</a></p>';
                         ?>
 					</div>
 					<?php

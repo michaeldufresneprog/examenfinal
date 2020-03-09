ae5523805c166fe58e325bbc91df6fde112f674d
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-03-09 08:05:05 -0400

    folder enfant

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version'));
+    wp_enqueue_script( 'main_js',get_stylesheet_directory_uri() . '/js/main.js');
+    
+}
+
+
+function wpdocs_custom_excerpt_length() {
+    return 15;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+
+?>
+
